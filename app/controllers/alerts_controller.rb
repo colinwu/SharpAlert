8bb73b6be6c8a172b75b26611cd2d5af480a0ca1
Colin Wu <colin@colinwu.ca>
2013-01-22 10:20:48 -0500

    Fixed "Maintenance" column label in notify_controls/index.html.erb. Cosmetic fixups in notify_contorls edit form. Alerts/index method now sorts by "#{params[:sort]},alert_date"

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -17,7 +17,7 @@ class AlertsController < ApplicationController
     if params[:sort].nil?
       sort = 'alert_date'
     else
-      sort = params[:sort]
+      sort = "#{params[:sort]},alert_date"
     end
     unless (params[:commit].nil?)
       where_array = Array.new
@@ -26,25 +26,21 @@ class AlertsController < ApplicationController
         where_array << 'device_name regexp ?'
         @name_q = params[:name_q]
         condition_array << @name_q
-#         @alerts = Alert.paginate(:page => params[:page], :order => sort, :conditions => ['device_name regexp ?', @name_q], :per_page => 30)
-      end
+     end
       if(not params['model_q'].empty?)
         where_array << 'device_model regexp ?'
         @model_q = params[:model_q]
         condition_array << @model_q
-#         @alerts = Alert.paginate(:page => params[:page], :order => sort, :conditions => ['device_model regexp ?', @model_q], :per_page => 30)
       end
       if(not params['serial_q'].empty?)
         where_array << 'device_serial regexp ?'
         @serial_q = params[:serial_q]
         condition_array << @serial_q
-#         @alerts = Alert.paginate(:page => params[:page], :order => sort, :conditions => ['device_serial regexp ?', @serial_q], :per_page => 30)
       end
       if(not params['msg_q'].empty?)
         where_array << 'alert_msg regexp ?'
         @msg_q = params[:msg_q]
         condition_array << @msg_q
-#         @alerts = Alert.paginate(:page => params[:page], :order => sort, :conditions => ['alert_msg regexp ?', @msg_q], :per_page => 30)
       end
       if not where_array.empty?
         condition_array[0] = where_array.join(' and ')
