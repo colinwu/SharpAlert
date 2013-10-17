dc7d8b0a9a6c74962d0fbc652fc00c7734f0c519
Colin <colin@colinwu.ca>
2013-10-17 10:01:08 -0400

    Click on special alert freqency report numbers to view alert details. Preparing to generate charts based on special reports.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+//= require highcharts/highcharts                                                           
+//= require highcharts/highcharts-more                                                         
+//= require highcharts/highstock
