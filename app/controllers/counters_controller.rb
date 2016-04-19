162cedf577179cfdadad0dd2d38e8d5107d0ba6d
Colin <colin@colinwu.ca>
2016-04-19 16:13:34 -0400

    Fix bug in counters

--- app/controllers/counters_controller.rb
+++ app/controllers/counters_controller.rb
@@ -33,7 +33,7 @@ class CountersController < ApplicationController
     @lifetime = Hash.new
     
     @devices.each do |d|
-      @lifetime[d.device_id] = d.device.print_volume.lifetime
+      @lifetime[d.device_id] = d.device.print_volume.nil? ? nil : d.device.print_volume.lifetime
       if (params[:end_q].nil? or params[:end_q].empty?)
         @last[d.device_id] = Counter.latest(d.device_id)
       else
